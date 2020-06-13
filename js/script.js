12f89787128290c9c06ddc27152ff085e25340a4
Ashwin Vinod <ashwivinod16@gmail.com>
2020-06-13 22:56:34 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const navSlide = () =>{
+    const icon= document.querySelector('.navbar-icon')
+    const nav = document.querySelector('.navlinks')
+
+    // Toggle Nav
+    nav.classList.toggle('nav-active') 
+}
